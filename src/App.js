5204ea795ccbbe3a5696c79990df4cafec895048
Eswar S K <eswar.sk413@gmail.com>
2024-09-24 00:19:35 +0530

    New Changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import XTable from './XTable';
+
+function App() {
+  return (
+    <>
+      <XTable />
+    </>
+  );
+}
+
+export default App;
